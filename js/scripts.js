e904c0260d51e804dcedc8981a458f3fdb076be0
urenami <122377715+urenami@users.noreply.github.com>
2023-01-29 20:16:39 -0500

    added height and type to pokemon
    in this code i included the height to each pokemon with their type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 
 let  pokemonList= [
-    { name:'charizard' },
-    { name:'Blastoise' },
-    { name:'Venusar' },
-    { name:'Nidoqueen' },
-    { name:'Nidoking' },
-    { name:'Blaziken' },
+    { name: "charizard", height: 1.7, type: ["fire", "flying"] },
+    { name: "Blastoise", height: 1.6, type: "water" },
+    { name: "Venusaur", height: 2, type: ["grass", "poison"] },
+    { name: "Nidoqueen", height: 1.3, type: ["ground", "poison"] },
+    { name: "Nidoking", height: 1.4, type:["ground", "poison"] },
+    { name: "Blaziken", height: 1.9, type: ["fire", "fighting"] },
 ]
 document.write(pokemon.name + pokemonList.height)
