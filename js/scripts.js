de2156bb6887bbd3fc76b498487748f7ea763a8f
urenami <122377715+urenami@users.noreply.github.com>
2023-01-29 19:35:25 -0500

    Updated pokemon list
    This will create the variable with pokemon name inside of array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello world');
-let  favoriteFood= 'Pizza';
-document.write(favoriteFood)
+
+let  pokemonList= [
+    { name:'charizard' },
+    { name:'Blastoise' },
+    { name:'Venusar' },
+    { name:'Nidoqueen' },
+    { name:'Nidoking' },
+    { name:'Blaziken' },
+]
+document.write(pokemon.name + pokemonList.height)
