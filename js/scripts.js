3b85b17a1d07732c61fdd715469a5fc9c733a609
urenami <122377715+urenami@users.noreply.github.com>
2023-02-08 10:15:55 -0500

    part 2 of task 1.5
    wrapped pokemonList in IIFE with assigned keys,  updated loop code with new changes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
-
-let  pokemonList= [
+let pokemonRepository = (function () {
+ 
+    let  pokemonList= [
     { name: "charizard", height: 1.7, type: ["fire", "flying"] },
     { name: "Blastoise", height: 1.6, type: "water" },
     { name: "Venusaur", height: 2, type: ["grass", "poison"] },
     { name: "Nidoqueen", height: 1.3, type: ["ground", "poison"] },
     { name: "Nidoking", height: 1.4, type:["ground", "poison"] },
     { name: "Blaziken", height: 1.9, type: ["fire", "fighting"] },
-]
+];
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+function getAll() {
+    return pokemonList;
+}
+
+return {
+    add: add,
+    getAll: getAll
+    };
+})();
 
 /* replacing for loop with for each loop*/ 
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
 
     if (pokemon.height >=2) {
         document.write(pokemon.name + " (height: " + pokemon.height + "); - Wow, that is a big pokemon!" + "<br>")
